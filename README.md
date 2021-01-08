67ad75645d64c217f303abced45a159b5709b469
Konstantinos Mazgaltzidis <58148103+konmaz@users.noreply.github.com>
2021-01-08 16:12:08 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # changeComitAuthorBash
-This Bash Script reads the names of the repositories. Then it changes the author name, email for every commit in the repositories. WARNING This script uses 'git commit -f" USE IT ON YOUR OWN RISK
+This Bash Script reads the names of the repositories. Then it changes the author name, email for every commit in the repositories.
+
+
+WARNING : These scripts come without warranty of any kind. Use them at your own risk. I assume no liability for the accuracy, correctness, completeness, or usefulness of any information provided by this site nor for any sort of damages using these scripts may cause.
+
+
---
+++ script.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+while true; do
+    read -p "This script uses the very powerful command 'git commit -f'. It can mess your GitHub repositories. Do you understand the consequences and want to continue? (yes\no): " yn
+    case $yn in
+        [Yy]* )break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+echo "STARING";
+mkdir -p tempRepos;
+cp 'repos.txt' 'tempRepos'
+cd tempRepos;
+mkdir -p backups;
+input="repos.txt";
+while IFS= read -r line
+do
+  printf "\n-----------\nFor repo : '%s':\n" : $line
+  if [[$line]]
+  then
+    printf "Repository already exist in tempRepos\n"
+  fi
+  git clone git@github.com:$line
+  if ! test -f $line+'zip'; then
+  	printf "%s backup dont exists.Creating....\n" : $line
+fi
+done < "$input"
+
